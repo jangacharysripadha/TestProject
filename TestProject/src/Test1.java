0659fdfe1a4301cabbe0dd23c1f7f882010a6b57
jangacharysripadha <schary1391@gmail.com>
2022-01-27 17:15:40 +0530

    test1 java class modified

--- TestProject/src/Test1.java
+++ TestProject/src/Test1.java
@@ -2,8 +2,8 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("welcome java world");
-
+		System.out.println("welcome to  java world");
+        System.out.println("This is a java api");
 	}
 
 }
