5d48736d9cbb94c8c80577613f9c60c744a25925
jangacharysripadha <schary1391@gmail.com>
2022-01-27 17:55:00 +0530

    spring boot file modified

---
+++ SpringBootRestApi/src/main/java/com/chary/springboot/SpringBootRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.chary.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+		System.out.println("welcome to spring boot rest api..............");
+	}
+
+}
